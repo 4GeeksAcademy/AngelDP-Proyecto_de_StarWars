6ca2452b6b77624f5289e68199669e75d5c83d7b
Angel Ponte <angeld0606@gmail.com>
2025-02-26 15:28:15 +0000

    Primera toma de contacto

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,9 +6,7 @@ import {
     Route,
 } from "react-router-dom";
 import { Layout } from "./pages/Layout";
-import { Home } from "./pages/Home";
-import { Single } from "./pages/Single";
-import { Demo } from "./pages/Demo";
+
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -22,9 +20,9 @@ export const router = createBrowserRouter(
       <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
-        <Route path= "/" element={<Home />} />
-        <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
-        <Route path="/demo" element={<Demo />} />
+        <Route path= "/"  />
+        <Route path="/single/:theId"  />  {/* Dynamic route for single items */}
+        <Route path="/demo"  />
       </Route>
     )
 );
